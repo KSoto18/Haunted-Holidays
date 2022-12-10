a9182ce288b55c1c2d86abe32a7ba83450c5aaca
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-12-09 19:00:38 -0500

    change some queries

--- client/src/pages/SinglePlace.js
+++ client/src/pages/SinglePlace.js
@@ -7,13 +7,13 @@ import { useQuery } from '@apollo/client';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 
-import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
+import { QUERY_SINGLE_PLACE } from '../utils/queries';
 
 const Places= () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { placeId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, {
+  const { loading, data } = useQuery(QUERY_SINGLE_PLACE, {
     // pass URL parameter
     variables: { placeId: placeId },
   });
