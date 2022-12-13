15c899559d45cf93fa80952340010f8b497c88c4
Maggie Lively <maggielively@maggielvelyspro.lan>
2022-12-13 15:19:49 -0500

    sooooo many pins

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-
+import { QUERY_MARKERS } from '../../utils/queries';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 
 const MapContainer = () => {
-  
+  const { loading, data } = useQuery(QUERY_MARKERS);
+// console.log(data?.locations[0]);
+
+const locations = data?.locations;
+
   const mapStyles = {        
     height: "100vh",
     width: "100%"};
@@ -25,8 +29,24 @@ const MapContainer = () => {
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={5}
-          center={defaultCenter}
-        />
+          center={defaultCenter}>
+
+{/* <Marker key={"test"} position={{lat: 42.9621061, lng: -85.50489309999999}}/> */}
+
+             {
+            locations?.map(location => {
+              const locationObject = {
+                lat: parseInt(location.latitude),
+                lng: parseInt(location.longitude)
+              }
+              return (
+              <Marker key={location.location} position={locationObject}/>
+              )
+            })
+         }
+
+          </GoogleMap>
+        
      </LoadScript>
   )
 }
