568df15f07e8426a6285d9911b1847833d96340d
Karina Drummond <k.drummond528@gmail.com>
2022-12-13 13:52:34 -0800

    added a backtotop button

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import SingleReview from './pages/SingleReview';
 import Profile from './pages/Profile'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import BackToTopBtn from './components/BackToTopBtn';
 
 import About from './components/About';
 import Contact from './components/Contact';
@@ -52,20 +53,20 @@ function App() {
 
           <div>
             <Routes>
-              <Route path="/" 
-              element={<Home />} 
+              <Route path="/"
+                element={<Home />}
               />
 
-              <Route path="/login" 
-              element={<Login />} 
+              <Route path="/login"
+                element={<Login />}
               />
 
-              <Route path="/signup" 
-              element={<Signup />} 
+              <Route path="/signup"
+                element={<Signup />}
               />
 
-              <Route path="/about" 
-              element={<About />} 
+              <Route path="/about"
+                element={<About />}
               />
 
               <Route path="/contact"
@@ -75,20 +76,21 @@ function App() {
               <Route path="/profile"
                 element={<Profile />}
               />
-                {/* <Route path="/profiles/:username"
+              {/* <Route path="/profiles/:username"
                 element={<Profile />}
               /> */}
-              
+
               <Route
                 path="/reviews/:reviewId"
-                 element={<SingleReview />} 
-                 />
+                element={<SingleReview />}
+              />
 
             </Routes>
 
           </div>
 
           <Footer />
+          <BackToTopBtn />
         </div>
       </Router>
     </ApolloProvider>
