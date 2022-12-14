3b0b4a012e533c593ec3f273febebebd16387dbd
Squiddy <wolaki96@gmail.com>
2022-12-14 11:04:02 -0500

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,13 +8,14 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
+import HelpIcon from '@mui/icons-material/Help';
 
 
 const Footer = () => {
   const [value, setValue] = React.useState(0);
   const [modalOpen, setModalOpen] = useState(false);
   return (
-    
+
 
     <Box >
       <BottomNavigation sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: "black" }}
@@ -24,41 +25,25 @@ const Footer = () => {
           setValue(newValue);
         }}
       >
-         
-        <BottomNavigationAction  href='https://github.com/KSoto18/Haunted-Holidays'  label="Github" icon={<GitHubIcon />} />
 
-        <BottomNavigationAction   label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction href='https://github.com/KSoto18/Haunted-Holidays' label="Github" icon={<GitHubIcon />} />
 
-        <BottomNavigationAction  onClick={event =>  window.location.href='/profile'} label="Explore" icon={<LocationOnIcon />} />
-        <p className= "bottomCopyright">
-        © 2022 Four Loopin' Ladies
-      </p>
+        <BottomNavigationAction onClick={() => {
+          setModalOpen(true);
+        }} label="What's This?" icon={<HelpIcon />} />
+
+        <BottomNavigationAction onClick={event => window.location.href = '/profile'} label="Explore" icon={<LocationOnIcon />} />
+
+        <p className="bottomCopyright">
+          © 2022 Four Loopin' Ladies
+        </p>
       </BottomNavigation>
-     
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
+
     </Box>
   );
 }
-        
-//         onClick={() => {
-//           setModalOpen(true);
-//         }}
-//       >
-//         What's this?
-//       </Button>
 
-//       {modalOpen && <Modal setOpenModal={setModalOpen} />}
-//       <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, elevation: 3}}>
-//       <BottomNavigation sx={{background: "black"}}>
-//         <p className="footertag">Four Loopin Ladies</p>
-        
-   
-    
-   
-    
-//     </BottomNavigation>
-//     </Box>
-//     </footer>
-//   );
-// };
+
 
 export default Footer;
