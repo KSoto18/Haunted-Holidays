842db58074c015600eaf1e28deab54dd727decd1
Karina Drummond <k.drummond528@gmail.com>
2022-12-14 11:26:33 -0800

    modified components and css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiGhost } from 'react-icons/gi';
-// import Auth from '../../utils/auth';
+import Auth from '../../utils/auth';
 import SideNav from '../SideNav';
-
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Fade from '@mui/material/Fade';
-
+import { SlLogin, SlLogout } from 'react-icons/sl';
+import { FiUserPlus } from 'react-icons/fi';
 // import Whispering from '../../assets/mp3/whispering.mp3';
-
-// import { useTheme } from './ThemeContext';
-
-
 
 const Header = () => {
 
@@ -22,18 +14,10 @@ const Header = () => {
     Auth.logout();
   };
 
-// var scareEffect = document.getElementById('scare-sound');
+  // var scareEffect = document.getElementById('scare-sound');
   // const playEffect = () => {
   //   scareEffect.play();
   // }
-  // const [anchorEl, setAnchorEl] = React.useState(null);
-  // const open = Boolean(anchorEl);
-  // const handleClick = (event) => {
- //    setAnchorEl(event.currentTarget);
-  // };
- //  const handleClose = () => {
- //    setAnchorEl(null);
- //  };
 
   return (
     <div>
@@ -45,28 +29,52 @@ const Header = () => {
       </audio> */}
 
 
-      <div>
       <header>
-    
+
         {/* link to home in pg title  */}
         <Link className="headertitle" to="/">
-          <h1>Haunted Holidays </h1>
+          <h1>Haunted Holidays<GiGhost size={'4em'} /></h1>
         </Link>
-        <p style={{color: "offwhite" }}>
-        <GiGhost /><GiGhost /><GiGhost /> Find your next spooky experience! <GiGhost /><GiGhost /><GiGhost />
+        <p className='subheader' style={{ color: "offwhite" }}>
+          Find your next spooky experience!
         </p>
 
+        {/* login/signup/logout buttons */}
+        <div>
+
+          {Auth.loggedIn() ? (
+            <>
+              <div className='logout-btn-container'>
+                <p className='hello-user-msg'>
+                  Hello, {Auth.getProfile().data.username}!</p>
+                <div>
+                  <Link className="logout-btn"
+                    style={{ fontSize: '14px' }}
+                    onClick={logout}>
+                    Logout <SlLogout />
+                  </Link>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='login-signup-btn-container'>
+                <Link className="login-btn" to="/login">
+                  Login <SlLogin />
+                </Link>
+
+                <Link className="signup-btn" to="/signup">
+                  Signup <FiUserPlus />
+                </Link>
+              </div>
+            </>
           )}
 
-
-
         </div>
-       
 
+      </header>
+    </div>
 
-     
-   </div>
-    
   );
 };
 
