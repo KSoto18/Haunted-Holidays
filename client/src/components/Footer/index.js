8bcd628511f8304b439d9e62be45ffd9bc60e1b2
Squiddy <wolaki96@gmail.com>
2022-12-14 10:23:52 -0500

    added footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,38 +3,61 @@ import React, { useState } from 'react';
 import Modal from "../Modal/Modal";
 import Button from '@mui/material/Button';
 import BottomNavigation from '@mui/material/BottomNavigation';
-
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
 
 
 const Footer = () => {
-  
+  const [value, setValue] = React.useState(0);
   const [modalOpen, setModalOpen] = useState(false);
   return (
     
-    <footer>
-       <Button sx={{ background: "black",  position:"absolute",
-  bottom:100, left:"90%"}} variant = "contained"
-        
-        onClick={() => {
-          setModalOpen(true);
+
+    <Box >
+      <BottomNavigation sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: "black" }}
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
         }}
       >
-        What's this?
-      </Button>
+        <BottomNavigationAction  label="Github" icon={<GitHubIcon />} />
 
-      {modalOpen && <Modal setOpenModal={setModalOpen} />}
-      <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, elevation: 3}}>
-      <BottomNavigation sx={{background: "black"}}>
-        <p className="footertag">Four Loopin Ladies</p>
-   
-    
-   
-    
-    </BottomNavigation>
+        <BottomNavigationAction   label="Favorites" icon={<FavoriteIcon />} />
+
+        <BottomNavigationAction   label="Search" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </Box>
-    </footer>
   );
-};
+}
+//     <footer>
+//        <Button sx={{ background: "black",  position:"absolute",
+//   bottom:100, left:"90%"}} variant = "contained"
+        
+//         onClick={() => {
+//           setModalOpen(true);
+//         }}
+//       >
+//         What's this?
+//       </Button>
+
+//       {modalOpen && <Modal setOpenModal={setModalOpen} />}
+//       <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, elevation: 3}}>
+//       <BottomNavigation sx={{background: "black"}}>
+//         <p className="footertag">Four Loopin Ladies</p>
+        
+   
+    
+   
+    
+//     </BottomNavigation>
+//     </Box>
+//     </footer>
+//   );
+// };
 
 export default Footer;
